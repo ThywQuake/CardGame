8fcf5391805d95636a124578a1321f14a77fa09c
Thyw <3190841641@qq.com>
2026-01-09 20:43:22 +0800

    Delete core_bak

--- app/core_bak/entity/hero.py
+++
@@ -1,8 +0,0 @@
-from app.core_bak.config import HeroConfig
-from app.core_bak.entity.slot import Deck
-
-
-class Hero:
-    def __init__(self, **kwargs):
-        self.config: HeroConfig = kwargs.get("config", HeroConfig())
-        self.super_powers: Deck = kwargs.get("super_powers", Deck())
--- app/core_bak/event/__init__.py
+++
@@ -1,6 +0,0 @@
-from .event import Event
-from .event_manager import EventManager
-from .listener import Listener
-from .action import ActionManager
-
-__all__ = ["Event", "EventManager", "Listener", "ActionManager"]
